fb3fb891646401aa5bd051ec6c0d2175e79e4f8e
homer <hwhite0@georgetowncollege.edu>
2014-02-07 20:12:24 -0500

    hoping to remove that embedded null

--- R/ostrichtemp.R
+++ R/ostrichtemp.R
@@ -8,8 +8,9 @@
 #' \describe{ \item{body.temp}{carotid arterial body temperature of the ostrich (degrees Centigrade)}
 #' \item{brain.temp}{brain temperature (near hypothalamus) of the ostrich (degrees Centigrade)}
 #' }
-#' \source{Fuller et. al., Variability in brain and arterial blood temperature in free-rangin ostriches in their natural
+#' \source{Fuller et. al., Variability in brain and arterial blood temperature in free-ranging ostriches in their natural
 #' habitat", The Journal of Experimental Biology, 200:  pp. 1171 to 1181, April 2003.}
 #' \url{http://jeb.biologists.org/content/206/7/1171.long}
+#' }
 #' @keywords datasets
 #' NULL
