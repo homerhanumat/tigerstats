c3dc00f9ca5a58a6d7f05f127680e5127e7542e5
homer <hwhite0@georgetowncollege.edu>
2014-02-07 19:40:10 -0500

    hoping to remove that embedded null

--- R/ostrichtemp.R
+++ R/ostrichtemp.R
@@ -1,16 +1,15 @@
-#' Body and Brasin Temperatures of Ostriches
+#' Body and Brain Temperature of Ostriches
 #' 
-#' Body and brain temperatures of six free-ranging ostriches.
-#' 
+#' Body and brain temperature of siz free-ranging ostriches.
 #' 
 #' @name ostrichtemp
 #' @docType data
-#' @format A data frame with 6 observations on the following 2 variables.
-#' \describe{ \item{body.temp}{carotid arterial blood temperature of the ostrich (degrees Centigrade).}
-#' \item{brain.temp}{brain temperature (near hypothalumus) of the ostrich (degrees Centigrade).}}
-#' @source Fuller et. al., "Variability in brain and arterial blood temperatures in free-ranging
-#'  ostriches in their natural habitat",  The Journal of Experimental Biology 206: pp. 1171 to 1181.  April 2003.
-#'  \url{http://jeb.biologists.org/content/206/7/1171.long}.  Found in
-#'   Whitlock and Schluter, The Analysis of Biological Data.
+#' @format A data frame with 6 observations on the following 2 variables
+#' \describe{ \item{body.temp}{carotid arterial body temperature of the ostrich (degrees Centigrade)}
+#' \item{brain.temp}{brain temperature (near hypothalamus) of the ostrich (degrees Centigrade)}
+#' }
+#' \source{Fuller et. al., Variability in brain and arterial blood temperature in free-rangin ostriches in their natural
+#' habitat", The Journal of Experimental Biology, 200:  pp. 1171 to 1181, April 2003.}
+#' \url{http://jeb.biologists.org/content/206/7/1171.long}
 #' @keywords datasets
-NULL
+#' NULL
