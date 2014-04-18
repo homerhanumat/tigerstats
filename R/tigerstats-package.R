6954d6ac90625e89ac6cdf7f003a5270c6b4a5fa
homer <hwhite0@georgetowncollege.edu>
2014-04-17 21:23:55 -0400

    add tigerstats-package.R for roxygenation of package-level documentation

---
+++ R/tigerstats-package.R
@@ -0,0 +1,17 @@
+#' tigerstats
+#'
+#' Datasets and functions useful for teaching elementary statistics.
+#'
+#'
+#' @name tigerstats-package
+#' @aliases tigerstats tigerstats-package
+#' @docType package
+#' @title R for Elementary Statistics
+#' @author Rebekah Robinson (\email{Rebekah_Robinson@@georgetowncollege.edu}), Homer White (\email{hwhite0@@georgetowncollege.edu})
+#' @references
+#' \url{http://statistics.georgetowncollege.edu}
+#'
+#' @keywords package
+#' @importFrom stats binom.test t.test prop.test
+#' @import lattice
+NULL
