62da346029a13b8dffa5290d834758b7e98777ab
Homer White <hwhite0@georgetowncollege.edu>
2014-09-07 14:57:23 -0400

    Further work on barchartGC2 in sandbox, and fix percentage bug in barchartGC.

--- sandbox/barchartGC2.R
+++ sandbox/barchartGC2.R
@@ -2,21 +2,22 @@
 
 #' @description Wrapper for \code{\link{barchart}} in package \code{lattice}.  Creates a 
 #' barchart from raw data using formula-data syntax similar to that of \code{\link{xtabs}},
-#' or from table.  Defaults to a "standard"
+#' or from a table.  Defaults to a "standard"
 #' barchart in which the bars are vertical and unstacked.  Supports percentage barcharts.
 #' 
 #' @rdname barchartGC
-#' @usage barchartGC(x,data=parent.frame(),type="frequency",auto.key=TRUE,
+#' @usage barchartGC(x,data=parent.frame(),type="frequency",flat=FALSE,auto.key=TRUE,
 #'                        horizontal=FALSE,stack=FALSE,...)
 #' @param x Either a formula or an object that can be coerced to a table.  If formula, it must be 
 #' of the form ~var or ~var1+var2.
 #' @param data Usually a data frame that supplies the variables in \code{x}.  Variables not in the data
 #' argument are searched for in the parent environment.
 #' @param type Possible values are "frequency" and "percent".
+#' @param flat If set to TRUE, will produce barchart that resembles the layout of \code{xtabs}
 #' @param auto.key Provides a simple key
-#' @param horizontal Determines orientation of the bars
+#' @param horizontal Determines orientation of the bars (overrriden by flat)
 #' @param stacked Determines whether bars for tallies are stacked on eac other or placed
-#' next to one another
+#' next to one another (overrriden by flat)
 #' @param ... other arguments passed to \code{barchart}:  these include main, sub, and
 #' xlab, which are likely to be familiar to students from other \code{lattice} graphical
 #' functions.  An error is possible if other arguments
@@ -48,27 +49,62 @@
 #' #from tabulated data:
 #' dieTosses <- c(one=8,two=18,three=11,four=7,five=9,six=7)
 #' barchartGC(dieTosses,main="60 Rolls of a Die")
+#' 
+#' # a "flat" barchart, pictorial version of xtabs() 
+#' barchartGC(~sex+seat,data=m111survey,flat=TRUE,ylab="Sex")
+#' 
+#' # a "flat" barchart, pictorial version of xtabs() 
+#' barchartGC(~sex+seat,data=m111survey,type="percent",flat=TRUE,ylab="Sex")
 barchartGC <-
   function(x,data=parent.frame(),
-           type="frequency",auto.key=TRUE,horizontal=FALSE,stack=FALSE,...)  { 
+           type="frequency",flat=FALSE,auto.key=TRUE,horizontal=FALSE,stack=FALSE,...)  {
+    
+    levelTol <- 5 #above this number, set legend vertically to the right
+    
+    #handle incorrect type specifications
+    if (type %in% c("frequency","count","counts","freq","Freq","Counts","Count","Frequency")) {
+      type <- "frequency"
+    }
+    
+    if (type %in% c("percentage","percent","perc","%","Percentage","Percent","Perc")) {
+      type <- "percent"
+    }
+    
+    # handle arugments when user wants a barchart that looks like xtabs()
+    if (flat==TRUE) {
+      stack <- TRUE
+      horizontal <- TRUE
+    }
+    
+    
+    #handle user-specified x or y axis labels
     
     ellipses <- list(...)
     
-    if (type=="frequency") {
+    if (type=="frequency" && horizontal==FALSE) {
       if (is.null(ellipses$ylab)) {
-        ellipses$ylab <- "Freq"
+        ellipses$ylab <- "frequency"
       }
     }
       
-      if (type=="percent") {
+      if (type=="percent" && horizontal==FALSE) {
         if (is.null(ellipses$ylab)) {
-          ellipses$ylab <- "percentage"
+          ellipses$ylab <- "percent"
         }
       }
-        
-    otherArgs <- c(list(stack=stack,horizontal=horizontal,
-                        auto.key=auto.key),
-              ellipses)
+    
+    if (type=="frequency" && horizontal==TRUE) {
+      if (is.null(ellipses$xlab)) {
+        ellipses$xlab <- "frequency"
+      }
+    }
+    
+    if (type=="percent" && horizontal==TRUE) {
+      if (is.null(ellipses$xlab)) {
+        ellipses$xlab <- "percent"
+      }
+    }
+    
     
     if (is(x,"formula"))  { #we have a formula
       prsd <- ParseFormula(x)
@@ -78,6 +114,13 @@ barchartGC <-
         varname <- pullout[1]
         variable <- simpleFind(varName=varname,data=data)
         tab <- xtabs(~variable) 
+        
+        #set up the "other arguments" to be passed to barchart
+        otherArgs <- c(list(stack=stack,horizontal=horizontal,
+                            auto.key=auto.key),
+                       ellipses)
+        
+        
         if (type=="frequency") {
         args <- c(list(x=tab),otherArgs)
         return(do.call(barchart,args))
@@ -85,6 +128,7 @@ barchartGC <-
       
         if (type=="percent") {
         perctab <- 100*tab/sum(tab)
+        perctab[is.nan(perctab)] <- 0
         args <- c(list(x=perctab),otherArgs)
         return(do.call(barchart,args))
           }
@@ -97,12 +141,39 @@ barchartGC <-
         explanatory <- simpleFind(varName=expname,data=data)
         response <- simpleFind(varName=respname,data=data)
         tab <- table(explanatory,response)
+        
+        #make groups appear in order like xtabs
+        if (horizontal==TRUE) {
+          tab <- tab[nrow(tab):1,]
+        }
+        
+        #make legend work well with orientation of chart, if possible.  If
+        #there are lots of levels in exp variable, though, then put legend to the right
+        if (stack==FALSE && horizontal==FALSE && auto.key==TRUE) {
+          if (ncol(tab) <= levelTol) {
+          auto.key=list(space="top",columns=ncol(tab))
+          } else auto.key=list(space="right",columns=1)
+        }
+        
+        if (stack==TRUE && horizontal==TRUE && auto.key==TRUE) {
+          if (ncol(tab) <= levelTol) {
+            auto.key=list(space="top",columns=ncol(tab))
+          } else auto.key=list(space="right",columns=1)
+        }
+
+        
+        #set up the "other arguments" to be passed to barchart
+        otherArgs <- c(list(stack=stack,horizontal=horizontal,
+                            auto.key=auto.key),
+                       ellipses)
+        
         if (type=="frequency") {
         args <- c(list(x=tab),otherArgs)
         return(do.call(barchart,args))
         }
         if (type=="percent") {
           perctab <- 100*prop.table(tab,margin=1)
+          perctab[is.nan(perctab)] <- 0
           args <- c(list(x=perctab),otherArgs)
           return(do.call(barchart,args))
         }   
@@ -115,23 +186,56 @@ barchartGC <-
     if (!is(x,"formula")) {  #we have tabular data
       x <- as.table(x)
       if (length(dim(x))==1) {#one variable
+        
+        #set up the "other arguments" to be passed to barchart
+        otherArgs <- c(list(stack=stack,horizontal=horizontal,
+                            auto.key=auto.key),
+                       ellipses)
+        
         if (type=="frequency") {
           args <- c(list(x=x),otherArgs)
           return(do.call(barchart,args))
         }
         if (type=="percent") {
           perctab <- 100*x/sum(x)
+          perctab[is.nan(perctab)] <- 0
           args <- c(list(x=perctab),otherArgs)
           return(do.call(barchart,args))
         }     
       }
       if (length(dim(x))>1) {#two variables
+        
+        #make groups appear in order like xtabs
+        if (horizontal==TRUE) {
+          x <- x[nrow(x):1,]
+        }
+        
+        #make legend work well with orientation of chart, if possible.  If
+        #there are lots of levels in exp variable, though, then put legend to the right
+        if (stack==FALSE && horizontal==FALSE && auto.key==TRUE) {
+          if (ncol(x) <= levelTol) {
+            auto.key=list(space="top",columns=ncol(x))
+          } else auto.key=list(space="right",columns=1)
+        }
+        
+        if (stack==TRUE && horizontal==TRUE && auto.key==TRUE) {
+          if (ncol(x) <= levelTol) {
+            auto.key=list(space="top",columns=ncol(x))
+          } else auto.key=list(space="right",columns=1)
+        }
+        
+        #set up the "other arguments" to be passed to barchart
+        otherArgs <- c(list(stack=stack,horizontal=horizontal,
+                            auto.key=auto.key),
+                       ellipses)
+        
         if (type=="frequency") {
           args <- c(list(x=x),otherArgs)
           return(do.call(barchart,args))
         }
         if (type=="percent") {
           perctab <- 100*prop.table(x,margin=1)
+          perctab[is.nan(perctab)] <- 0
           args <- c(list(x=perctab),otherArgs)
           return(do.call(barchart,args))
       }
