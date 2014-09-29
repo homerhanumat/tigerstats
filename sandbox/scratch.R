e2168d2e18a87ebb91562b2f15b9424db0a7bdc6
Homer White <hwhite0@georgetowncollege.edu>
2014-09-29 12:47:12 -0400

    save scratch

--- sandbox/scratch.R
+++ sandbox/scratch.R
@@ -6,9 +6,9 @@ as.name("hello")
 res <-quote("hello")
 str(res)
 
-mod <- lmGC2(sheight~fheight,data=galton,degree=6,graph=TRUE)
+mod <- lmGC2(sheight~fheight,data=galton,degree=1,graph=TRUE)
 predict(mod,x=65)
 
 mod <- lmGC2(OBP~Season,data=henderson,degree=2,graph=TRUE)
 
-lmGC2(gasbill~temp,data=Utilities,degree=5,graph=TRUE)
+lmGC2(gasbill~temp,data=Utilities,degree=2,check=TRUE)
