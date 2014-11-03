067a1a06274c551ef72d453aee8d723cd3eb09f8
Homer White <hwhite0@georgetowncollege.edu>
2014-11-03 18:09:09 -0500

    refactoring chisqtestGC()

--- sandbox/scratch.R
+++ sandbox/scratch.R
@@ -2,3 +2,12 @@ mystuff <- binomtestGC(~sex,data=m111survey,success="female",p=0.5,graph=TRUE,co
 mystuff
 
 binomtestGC(x=40,n=71,p=0.5,alternative="greater",graph=T)
+
+x <- rep(c("a","b"),3)
+y <- as.character(c(1,1,1,2,2,2))
+x <- factor(x,levels=letters[1:3])
+y <- factor(y,levels=as.character(1:3))
+xtabs(~x+y)
+complete.cases(x,y)
+complete.cases(xtabs(~x+y))
+chisq.test(xtabs(~x+y))
