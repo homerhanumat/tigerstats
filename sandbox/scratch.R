d2c05bcd13a4ead70dbda57b023e9f8205ced6aa
Homer White <hwhite0@georgetowncollege.edu>
2014-12-11 10:29:00 -0500

    will scratch file go away

--- sandbox/scratch.R
+++ sandbox/scratch.R
@@ -1,5 +1,13 @@
-ttestGC(fastest~sex,m111survey,alternative="l")
+ttestGC(fastest~Sex,m111survey,alternative="l")
 proptestGC(~sex+love_first,m111survey,p=0,alternative="t")
 ChisqSimSlow(~weather+crowd.behavior,ledgejump,effects="f")
 res <- binomtestGC(~seat,m111survey,p=0.3,success="1_front",alternative="g")
 barchartGC(~sex+seat,m111survey,type="%",flat=T)
+
+fn <- function(data){
+  dataName <- deparse(substitute(data))
+  dataName
+}
+
+res <- chisqtestGC(~sex+seat,data=m111survey,simulate.p.value=T)
+densityplot(~Sims,data=res)
