75d72ba183d0b0174cbb84270dc6e0ec10a13287
Homer White <homerhanumat@gmail.com>
2016-06-11 16:18:29 -0400

    edit package statup message

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage(paste0("Welcome to my tigerstats!\n",
+  packageStartupMessage(paste0("Welcome to tigerstats!\n",
       "To learn more about this package, consult its website:\n",
       "\thttp://homerhanumat.github.io/tigerstats"))
 }
